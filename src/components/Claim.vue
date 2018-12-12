860bf3167b8abc485b0a9e90b49517d448bb8ff4
agata.okolska <agata.okolska@gmail.com>
2018-12-12 01:04:05 +0100

    Add basic Search view

---
+++ src/components/Claim.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="claimWrapper">
+      <h1 class="claim">THE UNIVERSE</h1>
+      <p class="subclaim">
+        Begin your yourney through our amazing galaxy,
+      and discover places you never even heard of.
+      </p>
+      <p class="subclaim">
+        Type anythig space-related to start
+      </p>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Claim.vue',
+  };
+</script>
+
+<style lang="scss" scoped>
+  .claimWrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  .claim {
+    margin: 0;
+    font-size:  50px;
+  }
+
+  subclime {
+    margin: 20px 0 0 0;
+    font-size:  15px;
+  }
+</style>
